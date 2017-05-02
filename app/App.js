9c829674a7212664d6f3b21665dfbc981f9a6d97
Edward Apostol <edward.apostol.github@gmail.com>
2017-05-02 00:08:47 -0400

    #021.00: code cleanup

--- app/App.js
+++ app/App.js
@@ -1,12 +1,6 @@
-/**
- * Created by Edward_J_Apostol on 2017-04-28.
- */
-
 export default class App {
     constructor()
     {
-        console.log("hello from App");
-        console.log("sdfdsfdsfdsf");
-        console.log("sdfdsfdsffssdfdsfsdf");
+        console.log("Hello World from the App");
     }
 }
